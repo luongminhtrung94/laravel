a86977490dd2f036afdfb132730726fbef3301e0
trung luong <>
2018-12-07 13:18:19 +0700

    no message

--- database/migrations/2018_12_06_073657_create_comments_table.php
+++ database/migrations/2018_12_06_073657_create_comments_table.php
@@ -16,10 +16,17 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->longText('body');
-            $table->string('url' , 255);
+            $table->string('url' , 255)->nullable();
             $table->integer('user_id')->unsigned();
             $table->integer('commentable_id')->unsigned();
             $table->string('commentable_type');
+
+
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('commentable_id')->references('id')->on('projects');
+            $table->foreign('company_id')->references('id')->on('companies');
+
             $table->timestamps();
         });
     }
